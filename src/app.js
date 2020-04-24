841cf2f538d2ed035a147c9227804293be0d65bf
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2020-04-24 12:38:16 -0300

    eslint config

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-function logRequest(request,response,next){
-  const {method,url} = request
+function logRequest (request, response, next) {
+  const { method, url } = request
 
   const logLabel = `[${method.toUpperCase()}] ${url}`
 
@@ -24,25 +24,25 @@ function logRequest(request,response,next){
 
 app.use(logRequest)
 
-app.get("/repositories", (request, response) => {
+app.get('/repositories', (request, response) => {
   return response.json(repositories)
-});
+})
 
-app.post("/repositories", (request, response) => {
+app.post('/repositories', (request, response) => {
   const body = request.body
-  const repository = {id:uuid(),...body,likes:0}
-  
+  const repository = { id: uuid(), ...body, likes: 0 }
+
   repositories.push(repository)
   return response.json(repository)
-});
+})
 
-app.put("/repositories/:id", (request, response) => {
-  const {id} = request.params
-  const {title,url,techs} = request.body
-  const repositoryIdx = repositories.findIndex(repository=>repository.id ===id)
-  
-  if(repositoryIdx<0){
-    return response.status(400).json({error:'Repository not found'})
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const repositoryIdx = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIdx < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
   }
   const repository = {
     ...repositories[repositoryIdx],
@@ -55,34 +55,32 @@ app.put("/repositories/:id", (request, response) => {
   repositories[repositoryIdx] = repository
 
   return response.json(repository)
+})
 
-});
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const repositoryIdx = repositories.findIndex(repository => repository.id === id)
 
-app.delete("/repositories/:id", (request, response) => {
-  const {id} = request.params
-  const repositoryIdx = repositories.findIndex(repository=> repository.id === id)
-
-  if(repositoryIdx<0){
-    return response.status(400).json({error:"Repository not found"})
+  if (repositoryIdx < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
   }
 
-  repositories.splice(repositoryIdx,1)
+  repositories.splice(repositoryIdx, 1)
 
   return response.status(204).send()
-});
+})
 
-app.post("/repositories/:id/like", (request, response) => {
-  const {id} = request.params
-  const repository = repositories.find(repository=>repository.id === id)
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params
+  const repository = repositories.find(repository => repository.id === id)
 
-  if(!repository){
-    return response.status(400).json({error: "Repository not found"})
+  if (!repository) {
+    return response.status(400).json({ error: 'Repository not found' })
   }
 
-  repository.likes+=1
+  repository.likes += 1
 
   return response.json(repository)
+})
 
-});
-
-module.exports = app;
+module.exports = app
