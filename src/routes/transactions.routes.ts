36fb0d66d68ef75e3980e9c9ec7b5dbaf4169b3d
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2020-05-06 14:06:06 -0300

    commit

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,48 @@
-import { Router } from 'express';
+import { Router } from 'express'
+import { getCustomRepository } from 'typeorm'
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import CreateTransactionService from '../services/CreateTransactionService'
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import DeleteTransactionService from '../services/DeleteTransactionService'
+import ImportTransactionsService from '../services/ImportTransactionsService'
 
-const transactionsRouter = Router();
+const transactionsRouter = Router()
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
-});
+  const transactionsRepository = getCustomRepository(TransactionsRepository)
+
+  const transactions = await transactionsRepository.find()
+  const balance = await transactionsRepository.getBalance()
+
+  return response.json({ transactions, balance })
+})
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
-});
+  const { title, value, type, category } = request.body
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
-});
+  const createTransaction = new CreateTransactionService()
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  })
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+  return response.json(transaction)
+})
 
-export default transactionsRouter;
+// transactionsRouter.delete('/:id', async (request, response) => {
+//   const { id } = request.params
+
+//   const deleteTransaction = new DeleteTransactionService()
+
+//   await deleteTransaction.execute(id)
+
+//   return response.status(204).send()
+// })
+
+// transactionsRouter.post('/import', async (request, response) => {
+//   // TODO
+// })
+
+export default transactionsRouter
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,15 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm'
+
+import AppError from '../errors/AppError'
+
+import Transaction from '../models/Transaction'
+
+import TransactionRepository from '../repositories/TransactionsRepository'
 
 class DeleteTransactionService {
   public async execute(): Promise<void> {
-    // TODO
+    const transactioRepository = new getCustomRepository(TransactionRepository)
   }
 }
 
-export default DeleteTransactionService;
+export default DeleteTransactionService
